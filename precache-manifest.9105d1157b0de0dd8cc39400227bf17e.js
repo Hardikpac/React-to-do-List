95c96786c257162e1b040b676ac4ecd3d0712747
Hardik Pachory <pachory42@gmail.com>
2020-09-03 00:21:18 +0530

    Updates

---
+++ precache-manifest.9105d1157b0de0dd8cc39400227bf17e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d960f631b799e011de03ff40e3664df",
+    "url": "/Hardikpac/React-to-do-List/index.html"
+  },
+  {
+    "revision": "c7cf6f12b6af52d9ae50",
+    "url": "/Hardikpac/React-to-do-List/static/css/main.f503aa8f.chunk.css"
+  },
+  {
+    "revision": "d25799c6b28b38e6576b",
+    "url": "/Hardikpac/React-to-do-List/static/js/2.12149c36.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Hardikpac/React-to-do-List/static/js/2.12149c36.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c7cf6f12b6af52d9ae50",
+    "url": "/Hardikpac/React-to-do-List/static/js/main.56483684.chunk.js"
+  },
+  {
+    "revision": "72d1a31ea69bc5067c95",
+    "url": "/Hardikpac/React-to-do-List/static/js/runtime-main.77179de0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Hardikpac/React-to-do-List/static/media/logo.5d5d9eef.svg"
+  }
+]);
